0398ab6fec945b9e6f485b47c941bd611fa60b8a
emiliozaat <emiliozaat@unisabana.edu.co>
2022-03-17 11:10:00 -0500

    Script
    Quedo relacionado con el gameplay

--- Proyecto Practico/Entregasegundorte/Assets/ControlCharacter.cs
+++ Proyecto Practico/Entregasegundorte/Assets/ControlCharacter.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControlCharacter : MonoBehaviour
-{
+public class ControlCharacter : MonoBehaviour {
     public GameObject characterPrincipal;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +13,6 @@ public class ControlCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      transform.position += Vector3.forward * Time.deltaTime;
+        
     }
 }
