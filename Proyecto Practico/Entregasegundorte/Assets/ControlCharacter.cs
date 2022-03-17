3f8a1b77058d583d48e0c57157798bf67b1ae59b
emiliozaat <emiliozaat@unisabana.edu.co>
2022-03-17 11:41:59 -0500

    Control para la derecha
    el personaje ya puede moverse a la derecha con el control

--- Proyecto Practico/Entregasegundorte/Assets/ControlCharacter.cs
+++ Proyecto Practico/Entregasegundorte/Assets/ControlCharacter.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class ControlCharacter : MonoBehaviour {
     public GameObject characterPrincipal;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
-
     // Update is called once per frame
     void Update() {
         //transform.position += Vector3.forward * Time.deltaTime;
         if (Input.GetKey(KeyCode.UpArrow)){
             transform.position += Vector3.forward * Time.deltaTime;
         }
-
         //Cuando presione la tecla DOWN el carro avanza hacia atras o en reversa
         if (Input.GetKey(KeyCode.DownArrow)){
             transform.position -= Vector3.forward * Time.deltaTime;
         }
         //Cuando presione la tecla RIGHT
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * Time.deltaTime;
+        }
+     
+
+    }
